6de0d7f06c41db3fa2c83c10d79d0de8ba67416f
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2023-02-05 19:25:57 +0530

    .

--- server.js
+++ server.js
@@ -3,64 +3,8 @@ const io = new Server(process.env.PORT || 9000, {
   cors: true,
 });
 
-// let nameToSocketMapping = new Map();
-// let socketToNameMapping = new Map();
-
-// io.on("connection", (socket) => {
-//   socket.on("get-me", () => {
-//     let id = socket.id
-//     socket.emit("me", {id});
-//   });
-
-//   socket.on("newRoom-created", (data) => {
-//     const { roomId, name } = data;
-//     nameToSocketMapping.set(name, socket.id);
-//     socketToNameMapping.set(socket.id, name);
-//     socket.join(roomId);
-//     socket.emit("newRoom-created", { roomId, name });
-//   });
-
-//   socket.on("join-room", (data) => {
-//     const { roomId, name } = data;
-//     nameToSocketMapping.set(name, socket.id);
-//     socketToNameMapping.set(socket.id, name);
-//     socket.join(roomId);
-//     socket.broadcast.to(roomId).emit("newUser-joined", { name });
-//   });
-
-//   socket.on("call-user", (data) => {
-//     const { name, offer } = data;
-//     const socketId = nameToSocketMapping.get(name);
-//     const fromName = socketToNameMapping.get(socket.id);
-//     socket.to(socketId).emit("incomming-call", { from: fromName, offer });
-//   });
-
-//   socket.on("call-accepted", (data) => {
-//     const { name, ans } = data;
-//     const socketId = nameToSocketMapping.get(name);
-//     socket.to(socketId).emit("calling-accepted", { ans });
-//   });
-
-//   socket.on("nego-call-user", (data) => {
-//     const { name, offer } = data;
-//     const socketId = nameToSocketMapping.get(name);
-//     const fromName = socketToNameMapping.get(socket.id);
-//     socket.to(socketId).emit("nego-incomming-call", { from: fromName, offer });
-//   });
-
-//   socket.on("nego-call-accepted", (data) => {
-//     const { name, ans } = data;
-//     const socketId = nameToSocketMapping.get(name);
-//     socket.to(socketId).emit("nego-calling-accepted", { ans });
-//   });
-// });
-
 io.on("connection", (socket) => {
-  // socket.emit("me", socket.id);
-  socket.on("get-me", () => {
-    let id = socket.id;
-    socket.emit("me", { id });
-  });
+  socket.emit("me", socket.id);
 
   socket.on("disconnect", () => {
     socket.broadcast.emit("callEnded");
